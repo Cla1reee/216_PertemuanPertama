a358286c1fcd46577971b4a7c2485848198d3272
Cla1reee <h.harimurti.ft23@mail.umy.ac.id>
2024-02-27 17:31:44 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Begin 
+    //numeric nAlas ,nTinggi ,nLuas
+    // Display 'Masukan Alas = '
+    // Accept nTinggi 
+    // Compute nLuas = nAlas*nTinggi/2
+    //Display 'Luasnya =' + nLuas
+    //End
+
+    float nAlas, nTinggi, nLuas;
+    cout << "Masukan Alas =";
+    cin >> nAlas;
+    cout << "Masukan Tinggi =";
+    cin >> nTinggi;
+    nLuas = nAlas * nTinggi / 2;
+    cout << "Luasnya = " << nLuas;
+
+    int panjang, lebar, luas;
+    cout << "Masukan Panjang = ";
+    cin >> panjang;
+    cout << "Masukan Lebar = ";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luas Persegi Panjang adalah =" << luas;
+
+
+}
+
